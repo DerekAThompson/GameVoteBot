b65fe3474b427fcb3a43f8a7535ea0ac02d1a4b7
DerekAThompson <derekt123@outlook.com>
2021-08-30 23:39:52 -0500

    Main Framework Developed
    Developed Main Framework Following Guide provided by DSharpPlus documentation on https://dsharpplus.github.io/articles/basics/first_bot.html

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace VoterBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "ODgxNzIyNzU0NjgzMDExMDcy.YSw-Tg.QYnkGH2Q19g6ACR-BLp2HZ8n8iw",
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
